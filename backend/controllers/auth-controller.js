86837ae9ea8d14494539179afdcea344ffa841ed
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-17 20:48:25 +0530

    Feat: Basic auth controller

---
+++ backend/controllers/auth-controller.js
@@ -0,0 +1,11 @@
+export const signUp = (req, res) => {
+  res.send("signUp");
+};
+
+export const signIn = (req, res) => {
+  res.send("signIn");
+};
+
+export const signOut = (req, res) => {
+  res.send("signOut");
+};
