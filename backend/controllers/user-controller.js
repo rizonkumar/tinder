3cda7387325f7b433fbb5bcda23e88454fad368c
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-18 21:29:49 +0530

    Feat: Implemented User  Update Profile Endpoint and Controller

---
+++ backend/controllers/user-controller.js
@@ -0,0 +1,18 @@
+const { default: cloudinary } = require("../config/cloudinary");
+const userService = require("../services/user-service");
+const AppError = require("../utils/appError");
+const sendResponse = require("../utils/responseHandler.JS");
+
+exports.updateProfile = async (req, res, next) => {
+  try {
+    const updatedUser = await userService.updateProfile(req.user._id, req.body);
+    sendResponse(res, 200, updatedUser, null, "Profile updated successfully");
+  } catch (error) {
+    if (error instanceof AppError) {
+      return res.status(error.statusCode).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  }
+};
