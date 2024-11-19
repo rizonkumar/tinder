77703b662068fe08612d1b145e17c4a51f87fd7d
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-19 08:25:13 +0530

    Feat: Added send and conversation routes

--- backend/routes/message-routes.js
+++ backend/routes/message-routes.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const { protectRoute } = require("../middleware/auth-middleware");
 
 const router = express.Router();
+
+router.use(protectRoute);
+
+router.post("/send", sendMessage);
+router.get("/conversation/:userID", getConversation);
 
 module.exports = router;
